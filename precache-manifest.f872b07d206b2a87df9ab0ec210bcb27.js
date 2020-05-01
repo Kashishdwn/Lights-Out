30a8c20a6a3c17b5086942dbcdb6fa6ceb415bdb
Kashishdwn <kashishdwn@gmail.com>
2020-05-02 03:31:39 +0530

    Updates

---
+++ precache-manifest.f872b07d206b2a87df9ab0ec210bcb27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082d245af3caa135840cec9bf55feda1",
+    "url": "/Lights-Out/index.html"
+  },
+  {
+    "revision": "8563f82679275b18b0e7",
+    "url": "/Lights-Out/static/css/main.e854b27b.chunk.css"
+  },
+  {
+    "revision": "04f954349b69b18b52c3",
+    "url": "/Lights-Out/static/js/2.18492b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lights-Out/static/js/2.18492b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8563f82679275b18b0e7",
+    "url": "/Lights-Out/static/js/main.7bfed09f.chunk.js"
+  },
+  {
+    "revision": "d199dcdf2cf92dbb4ff2",
+    "url": "/Lights-Out/static/js/runtime-main.443ebb8b.js"
+  }
+]);
